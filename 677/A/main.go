f5ed0ba4557c8d750cc1b927cd245e852cc1d2d9
Ilya Sudakov <isudakoff@gmail.com>
2021-02-09 10:38:54 +0700

    Release 677 - A

---
+++ 677/A/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func intScanln(n int) ([]int, error) {
+    x := make([]int, n)
+    y := make([]interface{}, len(x))
+
+    for i := range x {
+        y[i] = &x[i]
+    }
+
+    n, err := fmt.Scanln(y...)
+
+    x = x[:n]
+
+    return x, err
+}
+
+func main() {
+
+	var n, h, w int
+
+	fmt.Scanf("%d %d\n", &n, &h)
+
+    a, _ := intScanln(n)
+
+    for i := 0; i < n; i++ {
+        if (a[i] > h) {
+            w += 2
+        } else {
+            w += 1
+        }
+    }
+
+    fmt.Println(w)
+
+}
