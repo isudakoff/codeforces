7dd224dea8667d97dbfb0d7dda0b02487001541f
Ilya Sudakov <ilya.sudakov@skillbox.ru>
2021-10-23 22:40:36 +0200

    Release 520 - A

---
+++ 520/A/main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"unicode"
+)
+
+var scanner *bufio.Scanner
+var writer *bufio.Writer
+
+var (
+	localEnv       = flag.Bool("local", false, "Enable Local environment")
+	inputFileName  = flag.String("input", "input.txt", "Input filename")
+	outputFileName = flag.String("output", "output.txt", "Output filename")
+)
+
+func main() {
+	flag.Parse()
+
+	setScanner()
+	setWriter()
+
+	defer writer.Flush()
+
+	n := getInt()
+	s := getString()
+	alphabet := map[rune]int{
+		'A': 0,
+		'B': 0,
+		'C': 0,
+		'D': 0,
+		'E': 0,
+		'F': 0,
+		'G': 0,
+		'H': 0,
+		'I': 0,
+		'J': 0,
+		'K': 0,
+		'L': 0,
+		'M': 0,
+		'N': 0,
+		'O': 0,
+		'P': 0,
+		'Q': 0,
+		'R': 0,
+		'S': 0,
+		'T': 0,
+		'U': 0,
+		'V': 0,
+		'W': 0,
+		'X': 0,
+		'Y': 0,
+		'Z': 0,
+	}
+
+	for i := 0; i < n; i++ {
+		ch := unicode.To(unicode.UpperCase, rune(s[i]))
+
+		alphabet[ch]++
+	}
+
+	for _, i := range alphabet {
+		if i == 0 {
+			writeString("NO")
+			return
+		}
+	}
+
+	writeString("YES")
+}
+
+func toChar(i int) rune {
+	return rune('A' - 1 + i)
+}
+
+func getString() string {
+	scanner.Scan()
+
+	return scanner.Text()
+}
+
+func getInt64() int64 {
+	scanner.Scan()
+
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	return x
+}
+
+func getInt() int {
+	return int(getInt64())
+}
+
+func setScanner() {
+	if *localEnv {
+		inputFile, err := os.Open(*inputFileName)
+
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+
+		scanner = bufio.NewScanner(inputFile)
+	} else {
+		scanner = bufio.NewScanner(os.Stdin)
+	}
+}
+
+func setWriter() {
+	if *localEnv {
+		outputFile, err := os.Create(*outputFileName)
+
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+
+		writer = bufio.NewWriter(outputFile)
+	} else {
+		writer = bufio.NewWriter(os.Stdout)
+	}
+}
+
+func writeString(str string) {
+	_, _ = writer.WriteString(fmt.Sprintf("%s\n", str))
+}
+
+func writeInt(d int) {
+	_, _ = writer.WriteString(fmt.Sprintf("%d\n", d))
+}
+
+func writeInt64(d int64) {
+	_, _ = writer.WriteString(fmt.Sprintf("%d\n", d))
+}
+
+func writeFloat64(f float64) {
+	_, _ = writer.WriteString(fmt.Sprintf("%.2f\n", f))
+}
