6bf50674f118bee345a2011d136557ab7f18338d
Ilya Sudakov <ilya.sudakov@skillbox.ru>
2021-10-23 18:26:59 +0200

    Release 144 - A

---
+++ 144/A/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+var scanner *bufio.Scanner
+var writer *bufio.Writer
+
+var (
+	localEnv       = flag.Bool("local", false, "Enable Local environment")
+	inputFileName  = flag.String("input", "input.txt", "Input filename")
+	outputFileName = flag.String("output", "output.txt", "Output filename")
+)
+
+func main() {
+	flag.Parse()
+
+	setScanner()
+	setWriter()
+
+	defer writer.Flush()
+
+	scanner.Split(bufio.ScanWords)
+
+	max, max_i, min, min_i, res := 0, 0, 100, 100, 0
+	n := getInt()
+	a := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		a[i] = getInt()
+
+		if a[i] > max {
+			max = a[i]
+			max_i = i
+		}
+
+		if a[i] <= min {
+			min = a[i]
+			min_i = i
+		}
+	}
+
+	if max_i != min_i {
+		res += max_i
+	}
+
+	if min_i < n {
+		if min_i < max_i {
+			res += -(min_i + 2 - n)
+		} else {
+			res += -(min_i + 1 - n)
+		}
+	}
+
+	writeInt(res)
+}
+
+func getString() string {
+	scanner.Scan()
+
+	return scanner.Text()
+}
+
+func getInt64() int64 {
+	scanner.Scan()
+
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	return x
+}
+
+func getInt() int {
+	return int(getInt64())
+}
+
+func setScanner() {
+	if *localEnv {
+		inputFile, err := os.Open(*inputFileName)
+
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+
+		scanner = bufio.NewScanner(inputFile)
+	} else {
+		scanner = bufio.NewScanner(os.Stdin)
+	}
+}
+
+func setWriter() {
+	if *localEnv {
+		outputFile, err := os.Create(*outputFileName)
+
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+
+		writer = bufio.NewWriter(outputFile)
+	} else {
+		writer = bufio.NewWriter(os.Stdout)
+	}
+}
+
+func writeString(str string) {
+	_, _ = writer.WriteString(fmt.Sprintf("%s\n", str))
+}
+
+func writeInt(d int) {
+	_, _ = writer.WriteString(fmt.Sprintf("%d\n", d))
+}
+
+func writeInt64(d int64) {
+	_, _ = writer.WriteString(fmt.Sprintf("%d\n", d))
+}
+
+func writeFloat64(f float64) {
+	_, _ = writer.WriteString(fmt.Sprintf("%.2f\n", f))
+}
