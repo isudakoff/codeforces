66bafabd017b53ab63c229771d6e79e0de9d3c37
Ilya Sudakov <ilya.sudakov@skillbox.ru>
2021-10-24 13:32:19 +0200

    Release 581 - A

---
+++ 581/A/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+var scanner *bufio.Scanner
+var writer *bufio.Writer
+
+var (
+	localEnv       = flag.Bool("local", false, "Enable Local environment")
+	inputFileName  = flag.String("input", "input.txt", "Input filename")
+	outputFileName = flag.String("output", "output.txt", "Output filename")
+)
+
+func main() {
+	flag.Parse()
+
+	setScanner()
+	setWriter()
+
+	defer writer.Flush()
+
+	scanner.Split(bufio.ScanWords)
+
+	a := getInt()
+	b := getInt()
+
+	min := int(math.Min(float64(a), float64(b)))
+	a = a - min
+	b = b - min
+	max := int(math.Max(float64(a), float64(b)))
+
+	_, _ = writer.WriteString(fmt.Sprintf("%d %d\n", min, max/2))
+}
+
+func getInt64() int64 {
+	scanner.Scan()
+
+	x, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+
+	return x
+}
+
+func getInt() int {
+	return int(getInt64())
+}
+
+func setScanner() {
+	if *localEnv {
+		inputFile, err := os.Open(*inputFileName)
+
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+
+		scanner = bufio.NewScanner(inputFile)
+	} else {
+		scanner = bufio.NewScanner(os.Stdin)
+	}
+}
+
+func setWriter() {
+	if *localEnv {
+		outputFile, err := os.Create(*outputFileName)
+
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+
+		writer = bufio.NewWriter(outputFile)
+	} else {
+		writer = bufio.NewWriter(os.Stdout)
+	}
+}
